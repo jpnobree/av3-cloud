4fd75c67eee917d7b44027ec099caa26421e9548
joão pedro nobre <81717657+jpnobree@users.noreply.github.com>
2025-11-26 18:26:27 -0300

    Fix supabase client import statement

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { createClient } = require('@supabase/supabase-js');
+const supabaseClient = require('@supabase/supabase-js');
 
 const supabase = 
     supabaseClient.createClient('https://kdjcmoqqccmrcxmqoipc.supabase.co', 
